9205cbbe95e00984d885bcefe9df41caea6f64d1
tanawinttp <tanawin.ttp@gmail.com>
2023-09-04 23:56:08 +0700

    sec Hero Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/navbar";
+import Hero from "./components/hero"
 import "./App.css";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 }
