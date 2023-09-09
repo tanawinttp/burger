59fb5d80faa9cb836e46964a2bbed08d967d812c
tanawinttp <tanawin.ttp@gmail.com>
2023-09-09 16:47:37 +0700

    Add HeadlineCard

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Navbar from "./components/navbar";
 import Hero from "./components/hero"
+import HeadlineCards from "./components/HeadlineCards";
 import "./App.css";
+
+
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <HeadlineCards/>
     </div>
   );
 }
